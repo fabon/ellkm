14cfec37df36281d4a23a201b976b75eaed05422
fabon <dzogang.fabon@gmail.com>
2013-09-13 14:53:58 +0200

    gnu gpl v3

--- src/kmeans.hh
+++ src/kmeans.hh
@@ -1,3 +1,22 @@
+/**
+ ** Copyright 2013 Fabon Dzogang
+ **
+ ** This file is part of ellkm.
+
+ ** ellkm is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU General Public License as published by
+ ** the Free Software Foundation, either version 3 of the License, or
+ ** any later version.
+
+ ** ellkm is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU General Public License for more details.
+
+ ** You should have received a copy of the GNU General Public License
+ ** along with ellkm.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
 #ifndef K_MEANS_HH
 # define K_MEANS_HH
 
